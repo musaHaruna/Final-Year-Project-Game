de168db1f8c9e210871119053f91e29a025d27e8
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-07-18 02:16:08 +0300

    add-pop-sound

--- src/dashboard/layout/Sidebar.jsx
+++ src/dashboard/layout/Sidebar.jsx
@@ -2,8 +2,14 @@ import { NavLink } from 'react-router-dom'
 import Home from '../../assets/home.png'
 import Dumbbell from '../../assets/dumbbell.png'
 import Shop from '../../assets/shop.png'
+import PopSound from '../../assets/sounds/pop.mp3' // Import the pop sound
 
 const Sidebar = () => {
+  const playPopSound = () => {
+    const audio = new Audio(PopSound)
+    audio.play()
+  }
+
   return (
     <nav>
       <ul className='space-y-4 position-sidebar'>
@@ -16,6 +22,7 @@ const Sidebar = () => {
                 ? 'flex items-center space-x-2 text-blue-500 bg-blue-100 rounded-lg p-2'
                 : 'flex items-center space-x-2 text-gray-700 hover:bg-gray-100 rounded-lg p-2'
             }
+            onClick={playPopSound} // Add the onClick handler
           >
             <img src={Home} alt='Learn Icon' className='w-6 h-6' />
             <span className='uppercase'>Learn</span>
@@ -29,6 +36,7 @@ const Sidebar = () => {
                 ? 'flex items-center space-x-2 text-blue-500 bg-blue-100 rounded-lg p-2'
                 : 'flex items-center space-x-2 text-gray-700 hover:bg-gray-100 rounded-lg p-2'
             }
+            onClick={playPopSound} // Add the onClick handler
           >
             <img src={Dumbbell} alt='Practice Icon' className='w-6 h-6' />
             <span className='uppercase'>Practice</span>
@@ -43,6 +51,7 @@ const Sidebar = () => {
                 ? 'flex items-center space-x-2 text-blue-500 bg-blue-100 rounded-lg p-2'
                 : 'flex items-center space-x-2 text-gray-700 hover:bg-gray-100 rounded-lg p-2'
             }
+            onClick={playPopSound} // Add the onClick handler
           >
             <img src={Shop} alt='Shop Icon' className='w-6 h-6' />
             <span className='uppercase'>Shop</span>
