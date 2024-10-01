1cfd85ec51e4c0e22a200656ccf8241da56f0069
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-10-01 04:27:42 +0300

    fix-index

--- src/dashboard/pages/index.js
+++ src/dashboard/pages/index.js
@@ -2,6 +2,6 @@ import Learn from './Learn'
 import Practice from './Practice'
 import Shop from './Shop'
 import Home from './Home'
-import Signup from './SignUp'
-import Signin from './SignIn'
+import Signup from './Signup'
+import Signin from './Signin'
 export { Learn, Practice, Shop, Home, Signin, Signup }
